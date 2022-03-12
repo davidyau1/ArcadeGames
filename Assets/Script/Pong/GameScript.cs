c81961b1baa92b737c7b8b8cebb3e6566bc95cf3
David Yau <davidyau1@outlook.com>
2022-03-12 17:58:50 +1100

    Create Empty Functions and Fields for Pong

--- Assets/Script/Pong/GameScript.cs
+++ Assets/Script/Pong/GameScript.cs
@@ -4,11 +4,50 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    public enum State
+    {
+        Play,
+        GameOver,
+        Pause
+
+    }
+    public float playerSpeed;
+    public float ballSpeed;
+    public int[] score;
+
+
+    public GameObject ball;
+    public GameObject leftPlayer;
+    public GameObject rightPlayer;
+    public GameObject topWall;
+    public GameObject bottomWall;
+    public GameObject leftGoal;
+    public GameObject rightGoal;
+
+
+    void BeginRound()
+    {
+        Instantiate(ball);
+
+    }
+    void ScorePoint()
+    {
+        Destroy(ball);
+    }
+    void Collision()
+    {
+
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    
 
     // Update is called once per frame
     void Update()
