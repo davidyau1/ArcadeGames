1583b3e3c272d3d250b5c8d09167f89563138b4a
David Yau <davidyau1@outlook.com>
2022-03-14 18:22:16 +1100

    Add Ball Movement and Collision

--- Assets/Script/Pong/BallMovement.cs
+++ Assets/Script/Pong/BallMovement.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float speed;
+    public Vector3 direction;
+    void BeginRound()
+    {
+        ////Spawns the ball at (0,0)
+        //GameObject gameBall = Instantiate(ball, new Vector2(0, 0), Quaternion.identity);
+        //gameBall.GetComponent<BallMovement>();
+        //int side = Random.Range(0, 2);
+        //if (side == 0)
+        //{
+        //    ballDirection.x = -1;
+        //}
+        //else
+        //{
+        //    ballDirection.x = 1;
+        //}
+        //gameBall.transform.position = ballDirection;
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Debug.Log("We Hit Object");
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Wall")
+        {
+            direction.y *= -1;
+        }
+        if (collision.gameObject.tag == "Goal")
+        {
+            direction.x *= -1;
+
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            direction.x *= -1;
+
+        }
+
+    }
+
+    void Movement()
+    {
+        direction= direction.normalized;
+        rb.velocity = direction *  speed*Time.fixedDeltaTime ;
+
     }
 }
--- Assets/Script/Pong/GameScript.cs
+++ Assets/Script/Pong/GameScript.cs
@@ -1,57 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameScript : MonoBehaviour
 {
-    public enum State
+    public enum GameState
     {
-        Play,
+        Begin,
+        Playing,
         GameOver,
         Pause
 
     }
     public float playerSpeed;
-    public float ballSpeed;
     public int[] score;
-
-
-    public GameObject ball;
+    public GameObject ballPrefab;
     public GameObject leftPlayer;
     public GameObject rightPlayer;
     public GameObject topWall;
     public GameObject bottomWall;
     public GameObject leftGoal;
     public GameObject rightGoal;
+    public GameState state;
 
 
-    void BeginRound()
+    void StartRound()
     {
-        Instantiate(ball);
-
+        Instantiate(ballPrefab, new Vector3(0, 0, 0), Quaternion.identity);
     }
+
     void ScorePoint()
     {
-        Destroy(ball);
-    }
-    void Collision()
-    {
 
     }
+   
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (true)
+        {
+
+        }
+        StartRound();
     }
 
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
